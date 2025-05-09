d351e546060a06fae9f44d6a13c845db3426ba74
karmat314 <karmat314@gmail.com>
2025-05-09 11:12:23 +0600

    add basic login with static credentials

---
+++ lib/screens/DashboardScreen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'LoginScreen.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Dashboard"),),
+      body: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacement(MaterialPageRoute(
+              builder: (context) => const LoginScreen(),
+            ));
+          },
+          child: Text("Log out")
+      ),
+    );
+  }
+}
