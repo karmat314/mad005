87911d87497f59baf019079b45dbe2babdb8e3e1
karmat314 <karmat314@gmail.com>
2025-05-10 12:16:47 +0600

    add portfolio actions buttons for sharing,qr,etc...

--- lib/screens/portfolio_tab/PortfolioScreen.dart
+++ lib/screens/portfolio_tab/PortfolioScreen.dart
@@ -1,6 +1,8 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:mad005/screens/portfolio_tab/skills_widget.dart';
 import 'package:mad005/screens/portfolio_tab/training_widget.dart';
 import 'package:mad005/screens/portfolio_tab/work_history_widget.dart';
@@ -17,6 +19,7 @@ class PortfolioScreen extends StatefulWidget {
 
 class _PortfolioScreenState extends State<PortfolioScreen> {
   final FirebaseFirestore db = FirebaseFirestore.instance;
+  final _key = GlobalKey<ExpandableFabState>();
 
   @override
   Widget build(BuildContext context) {
@@ -32,6 +35,92 @@ class _PortfolioScreenState extends State<PortfolioScreen> {
     }
 
     return Scaffold(
+      floatingActionButtonLocation: ExpandableFab.location,
+      floatingActionButton: ExpandableFab(
+        key: _key,
+        type: ExpandableFabType.up,
+        overlayStyle: ExpandableFabOverlayStyle(
+          color: Colors.white.withOpacity(0.9),
+        ),
+        childrenAnimation: ExpandableFabAnimation.none,
+        distance: 70,
+        children: [
+          Row(
+            children: [
+              const Text('Share profile'),
+              const SizedBox(width: 20),
+              FloatingActionButton.small(
+                heroTag: null,
+                onPressed: () async {
+
+                },
+                child: const Icon(Icons.share),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              const Text('Show QR code'),
+              const SizedBox(width: 20),
+              FloatingActionButton.small(
+                heroTag: null,
+                onPressed: () async {
+
+                },
+                child: const Icon(Icons.qr_code),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              const Text('Export to LinkedIn'),
+              const SizedBox(width: 20),
+              FloatingActionButton.small(
+                heroTag: null,
+                onPressed: () {
+                },
+                child: const Icon(FontAwesomeIcons.linkedin),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              const Text('Export as PDF'),
+              const SizedBox(width: 20),
+              FloatingActionButton.small(
+                heroTag: null,
+                onPressed: () {
+                },
+                child: const Icon(FontAwesomeIcons.filePdf),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              const Text('Report profile'),
+              const SizedBox(width: 20),
+              FloatingActionButton.small(
+                heroTag: null,
+                onPressed: () {
+                },
+                child: const Icon(FontAwesomeIcons.flag),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              const Text('Claim this Profile'),
+              const SizedBox(width: 20),
+              FloatingActionButton.small(
+                heroTag: null,
+                onPressed: () {
+                },
+                child: const Icon(FontAwesomeIcons.shield),
+              ),
+            ],
+          ),
+        ],
+      ),
       body: SafeArea( // Optional: avoids notches/top bar
         child: SingleChildScrollView( // FIX: makes the screen scrollable
           child: Padding(
